cc4a0f7b6c4a29f9ce0760523243a1f97f05dc9c
John Ray <john@trychec.com>
2020-02-05 23:09:58 -0500

    add titles to stories, install knobs, export package content

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
 import Components from './components'
+=======
+import { BaseButton } from './components'
+>>>>>>> add titles to stories, install knobs, export package content
 import config from './../tailwind.config.js';
 
 module.exports = {
   config,
+<<<<<<< HEAD
   ...Components
+=======
+  BaseButton
+>>>>>>> add titles to stories, install knobs, export package content
 };
