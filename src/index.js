a0bfcfe13c2d1016d9f9b0eb9976eeff217a4b23
John Ray <john@trychec.com>
2020-02-06 12:21:26 -0500

    completely remove outline, apply custom focus, active, and hover styling

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import Components from './components'
-=======
-import { BaseButton } from './components'
->>>>>>> add titles to stories, install knobs, export package content
+import Components from './components';
 import config from './../tailwind.config.js';
 
 module.exports = {
   config,
-<<<<<<< HEAD
   ...Components
-=======
-  BaseButton
->>>>>>> add titles to stories, install knobs, export package content
 };
