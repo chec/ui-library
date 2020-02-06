609899067379549f6b10c8ff9e36cf5b41bb02c3
John Ray <john@trychec.com>
2020-02-06 16:52:17 -0500

    install vue-svg-loader to use svg files as Vue components

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
+};
