ad841d518640b0ed17e848cbf850b20828c5feff
John Ray <john@trychec.com>
2020-02-06 21:27:01 -0500

    show modification of default width style

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -3,51 +3,76 @@
     class="input"
     :value="value"
     :placeholder="placeholder"
-    :disabled="disabled"
-    :class="[
-      disabled && 'opacity-50 input--disabled',
-      error && 'input--error'
-     ]"
-    @input="$emit('input', $event.target.value)"
+    :disabled="this.variant === 'disabled'"
+    :class="classNames"
+    @input="onInput"
     />
 </template>
 <script>
 export default {
   name: 'TextField',
-  props: ['value', 'disabled', 'error', 'placeholder']
+  props: {
+    /**
+     * The state of the text field. One of "disabled", "error".
+     */
+    variant: {
+      type: String,
+      default: ''
+    },
+    /**
+     * The value the v-model directive utilizes, see https://vuejs.org/v2/guide/components.html#Using-v-model-on-Components
+     */
+    value: {
+      type: String,
+      default: ''
+    },
+    /**
+     * The value used for the inner `<input>`'s placeholder html attribute
+     */
+    placeholder: {
+      type: String,
+      default: 'Text Field'
+    }
+  },
+  computed: {
+    classNames() {
+      return {
+        'opacity-50 input--disabled': this.variant === 'disabled',
+        'input--error': this.variant === 'error'
+      }
+    }
+  },
+  methods: {
+    onInput($event) {
+      /**
+       * Emitted when the `<input>`'s 'input' event bubbles up. The v-model directive uses this to function.
+       * @event input
+       * @type {$event.target.value}
+       */
+      this.$emit('input', $event.target.value)
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
 .input {
   @apply text-base text-gray-500 p-4 bg-gray-200 rounded border border-gray-300 shadow-inner outline-none;
-  &:hover {
-    @apply border-primary-blue;
-  };
-  &:focus {
-    @apply border-primary-blue;
-  };
+  &:hover,
+  &:focus,
   &:active {
     @apply border-primary-blue;
   };
   &--disabled {
-    &:hover {
-      @apply border-gray-300;
-    };
-    &:focus {
-      @apply border-gray-300;
-    };
+    &:hover,
+    &:focus,
     &:active {
       @apply border-gray-300;
     };
   };
   &--error {
     @apply border-red-300;
-    &:hover {
-      @apply border-red-200;
-    };
-    &:focus {
-      @apply border-red-200;
-    };
+    &:hover,
+    &:focus,
     &:active {
       @apply border-red-200;
     };
