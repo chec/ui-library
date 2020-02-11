8bf2003dc8858e39d79fe1a132e05801adfc34f0
John Ray <john@trychec.com>
2020-02-11 17:10:10 -0500

    create BaseDropdown

---
+++ src/components/BaseDropdown.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dropdown">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseDropdown'
+};
+</script>
+
+<style lang="scss" scoped>
+  .dropdown {
+
+  }
+</style>
