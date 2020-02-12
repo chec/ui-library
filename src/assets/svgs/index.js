69cfcc2dfa5b0073ab513a3cf03eae9c4a59e9f6
John Ray <john@trychec.com>
2020-02-11 19:17:38 -0500

    add down-arrow svg

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,7 +1,9 @@
 import closeIcon from './close-icon.svg';
 import rightArrow from './right-arrow.svg';
+import downArrow from './down-arrown.svg';
 
 export default {
   closeIcon,
   rightArrow,
+  downArrow
 };
