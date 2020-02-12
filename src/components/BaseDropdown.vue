64f0f80e0afc5c2765bcc1d964dba70ff437761f
John Ray <john@trychec.com>
2020-02-12 11:06:41 -0500

    upgrade tailwindcss to 1.2 to support new utilities for transition properties

--- src/components/BaseDropdown.vue
+++ src/components/BaseDropdown.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="dropdown">
-    <div class="dropdown__control">
+    <div class="dropdown__control"
+      tabindex="0"
+      @click="onControlClick"
+      @keyup.enter="onControlClick"
+      @keyup.down="onControlClick">
       <span>
         Dropdown
         <div class="dropdown__down-arrow">
           <SvgDownArrow />
         </div>
       </span>
+    </div>
+    <div v-if="showDropdown" class="dropdown__menu">
+      <div v-for="(option, i) in options" :key="i" class="dropdown__option">
+        <span>{{option}}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -19,6 +28,24 @@ export default {
   components: {
     SvgDownArrow,
   },
+  props: {
+    /**
+     * The options for the dropdown
+     */
+    options: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      showDropdown: false,
+    };
+  },
+  methods: {
+    onControlClick() {
+      this.showDropdown = !this.showDropdown;
+    },
+  },
 };
 </script>
 
@@ -26,13 +53,26 @@ export default {
   .dropdown {
     @apply w-full;
     &__control {
-      @apply w-full bg-white shadow-sm rounded-sm;
+      @apply w-full bg-white shadow-sm rounded-sm outline-none transition-all ease-in duration-200 cursor-pointer;
+      &:hover {
+        @apply border border-gray-400;
+      }
+      &:focus,
+      &:active {
+        @apply border border-gray-500;
+      };
       > span {
-        @apply flex items-center justify-between w-full font-lato p-4 text-gray-500;
+        @apply flex items-center justify-between w-full font-lato p-4 text-sm text-gray-500;
       }
     }
+    &__menu {
+      @apply w-full bg-white border border-gray-300 rounded-sm;
+    }
+    &__option {
+      @apply font-lato text-sm text-gray-600 px-4 py-3;
+    }
     &__down-arrow {
-      @apply flex flex-col justify-center w-4 h-5 fill-current text-gray-600;
+      @apply flex flex-col justify-center w-5 h-5 fill-current text-gray-600;
     }
   }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -79,6 +79,13 @@ module.exports = {
       full: '9999px',
     },
     fontSize: fontSizes,
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+      '8': '8px',
+    },
     extend: {
       spacing: {
         ...fontSizes,
