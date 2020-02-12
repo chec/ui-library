6a0528480160785d95ee03a75d87ad7f6d06c6dc
John Ray <john@trychec.com>
2020-02-11 19:36:00 -0500

    add control element to dropdown block

--- src/components/BaseDropdown.vue
+++ src/components/BaseDropdown.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="dropdown">
-
+    <div class="dropdown__control">
+      <span>
+        Dropdown
+        <div class="dropdown__down-arrow">
+          <SvgDownArrow />
+        </div>
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
+import SvgDownArrow from '../assets/svgs/down-arrow.svg';
+
 export default {
-  name: 'BaseDropdown'
+  name: 'BaseDropdown',
+  components: {
+    SvgDownArrow,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
   .dropdown {
-
+    @apply w-full;
+    &__control {
+      @apply w-full bg-white shadow-sm rounded-sm;
+      > span {
+        @apply flex items-center justify-between w-full font-lato p-4 text-gray-500;
+      }
+    }
+    &__down-arrow {
+      @apply flex flex-col justify-center w-4 h-5 fill-current text-gray-600;
+    }
   }
 </style>
