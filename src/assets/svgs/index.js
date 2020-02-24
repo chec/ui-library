5116e138a880b82116ecb6b25ea9c7ab7f59d933
Robbie Averill <robbie@chec.io>
2020-02-25 08:11:01 +1300

    Replace custom made directive with vue-tooltip library

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,8 +1,7 @@
 import closeIcon from './close-icon.svg';
 import rightArrow from './right-arrow.svg';
-import triangle from './triangle.svg';
+
 export default {
   closeIcon,
   rightArrow,
-  triangle,
 };
--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,1 +1,7 @@
 import '../assets/tailwind.css';
+import Vue from 'vue';
+import { BaseTooltip } from '../index';
+
+Vue.directive('tooltip', BaseTooltip.VTooltip);
+Vue.directive('close-popover', BaseTooltip.VClosePopover);
+Vue.component('v-popover', BaseTooltip.VPopover);
