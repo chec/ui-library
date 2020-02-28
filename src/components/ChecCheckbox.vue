6b98fe307943df1808a563e52be46adc8cd7f0af
Jaeriah Tay <jaeriahpasseri@gmail.com>
2020-02-28 15:25:13 -0800

    added checkbox component
    added input styles
    
    fixed checkbox active and added checkbox group with disabled and label props

---
+++ src/components/ChecCheckbox.vue
@@ -0,0 +1,185 @@
+<template>
+    <div
+        class="checkbox"
+        :class="{ 'active' : isChecked, disabled }"
+    >
+      <label :for="id" class="checkbox__container">
+        <input
+            type="checkbox"
+            :id="id"
+            :name="name"
+            :value="value"
+            :checked="isChecked"
+            :disabled="disabled"
+            class="checkbox__input"
+            @input="handleInput"
+        />
+        <!--
+          @slot Custom label slot
+          @bind label string
+          @bind isChecked boolean
+          @bind disabled boolean
+        -->
+        <slot name="label" v-bind="{ label, isChecked, disabled }">
+          <div v-if="label" class="checkbox__label">{{ label }}</div>
+        </slot>
+        <span class="checkbox__checkmark" :class="{ 'is-active': isChecked }">
+            <chec-icon v-if="isChecked" icon="check" />
+        </span>
+      </label>
+    </div>
+</template>
+
+<script>
+import _uniqueId from 'lodash.uniqueid';
+import ChecIcon from './ChecIcon.vue';
+
+export default {
+  name: 'ChecCheckmark',
+  components: {
+    ChecIcon,
+  },
+  model: {
+    prop: 'selected',
+    event: 'input',
+  },
+  props: {
+    /**
+     * Name of the checkbox
+     */
+    name: {
+      type: String,
+      default: '',
+    },
+    /**
+     * Used to determine if the checkbox is selected
+     */
+    value: {
+      type: String,
+      default: '',
+    },
+    /**
+     * Sets the label for the input option.
+     */
+    label: {
+      type: String,
+      default: '',
+    },
+    /**
+     * States whether element is required
+     */
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Disables the checkbox
+     */
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Check if checkbox is selected.
+     */
+    selected: {
+      type: [Array, Boolean],
+      default: () => [],
+    },
+  },
+  computed: {
+    isChecked() {
+      if (typeof this.selected === 'boolean') {
+        return this.selected;
+      }
+      // Does the selected include the value, compare against the value
+      return this.selected.includes(this.value);
+    },
+    id() {
+      // generates unique id concating id, name, value
+      return _uniqueId(`${this.name || 'checkbox'}_${this.value}_`);
+    },
+  },
+  methods: {
+    handleInput() {
+      /**
+       * Emitted when input is selected.
+       * @event input
+       */
+      if (typeof this.selected === 'boolean') {
+        this.$emit('input', !this.selected);
+      } else {
+        // Store selected w/ spread operator on selected keys in array
+        let selected = [...this.selected];
+        if (selected.includes(this.value)) {
+          // uncheck
+          selected = selected.filter((value) => value !== this.value);
+        } else {
+          // check
+          selected.push(this.value);
+        }
+        this.$emit('input', selected);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+
+.checkbox {
+    @apply p-3 relative cursor-pointer;
+
+    &__container{
+       @apply flex font-lato text-sm text-gray-600 cursor-pointer;
+    }
+
+    // Hides browser's default radio style
+    &__input{
+      @apply
+        align-middle
+        flex
+        m-0
+        relative
+        appearance-none
+        bg-white
+        rounded-sm
+        border border-gray-400
+        shadow-sm
+        h-4 w-4 mt-1;
+
+      &:focus {
+          @apply outline-none;
+      }
+
+      &:hover{
+        @apply cursor-pointer bg-white border border-gray-500;
+      }
+
+      &:active{
+        @apply bg-gray-300 border border-gray-500;
+      }
+
+      &:disabled{
+        @apply bg-white border border-gray-300;
+      }
+
+      &:checked:after {
+        @apply text-white absolute;
+      }
+
+      &:checked{
+        @apply bg-gray-500 border-none;
+      }
+    }
+
+    &__checkmark .chec-icon{
+      @apply absolute;
+    }
+
+    &__label {
+        @apply pl-3;
+    }
+}
+
+</style>
