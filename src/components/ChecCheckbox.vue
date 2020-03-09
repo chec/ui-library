096e3bdcfcdd2be3621555448ac3825f2f1389af
Jaeriah Tay <jaeriahpasseri@gmail.com>
2020-03-09 16:54:59 -0700

    code refactoring on checkbox component according to pr comments

--- src/components/ChecCheckbox.vue
+++ src/components/ChecCheckbox.vue
@@ -1,41 +1,36 @@
 <template>
-    <div
-        class="checkbox"
-        :class="{ 'active' : isChecked, disabled }"
-    >
-      <label :for="id" class="checkbox__container">
-        <input
-            type="checkbox"
-            :id="id"
-            :name="name"
-            :value="value"
-            :checked="isChecked"
-            :disabled="disabled"
-            class="checkbox__input"
-            @input="handleInput"
-            :indeterminate="indeterminate"
-        />
-        <!-- Display none check icon and display when checked -->
-        <span v-show="!indeterminate && isChecked" class="checkbox__check">
-          <chec-icon icon="check" />
-        </span>
-        <!-- Display none minus icon and display when indeterminate -->
-        <span v-show="indeterminate" class="checkbox__minus">
-          <chec-icon icon="minus" />
-        </span>
-        <!--
-          @slot Custom label slot
-          @bind label string
-          @bind isChecked boolean
-          @bind disabled boolean
-        -->
-        <div v-if="label" class="checkbox__label" :class="{ disabled: disabled }">
-          <slot v-bind="{ label, isChecked, disabled }">
-            {{ label }}
-          </slot>
-        </div>
-      </label>
+  <label :for="id" class="checkbox" :class="{ 'active' : isChecked, disabled }">
+    <input
+      type="checkbox"
+      :id="id"
+      :name="name"
+      :value="value"
+      :checked="isChecked"
+      :disabled="disabled"
+      class="checkbox__input"
+      @input="handleInput"
+      :indeterminate="indeterminate"
+    />
+    <!-- Display none check icon and display when checked -->
+    <span v-show="!indeterminate && isChecked" class="checkbox__check">
+      <chec-icon icon="check" />
+    </span>
+    <!-- Display none minus icon and display when indeterminate -->
+    <span v-show="indeterminate" class="checkbox__minus">
+      <chec-icon icon="minus" />
+    </span>
+    <!--
+      @slot Custom label slot
+      @bind label string
+      @bind isChecked boolean
+      @bind disabled boolean
+    -->
+    <div v-if="label" class="checkbox__label" :class="{ disabled: disabled }">
+      <slot v-bind="{ label, isChecked, disabled }">
+        {{ label }}
+      </slot>
     </div>
+  </label>
 </template>
 
 <script>
@@ -43,7 +38,7 @@ import _uniqueId from 'lodash.uniqueid';
 import ChecIcon from './ChecIcon.vue';
 
 export default {
-  name: 'ChecCheckmark',
+  name: 'ChecCheckbox',
   components: {
     ChecIcon,
   },
@@ -64,7 +59,7 @@ export default {
      */
     value: {
       type: String,
-      default: '',
+      default: 'on',
     },
     /**
      * Sets the label for the input option.
@@ -99,7 +94,7 @@ export default {
      */
     selected: {
       type: [Array, Boolean],
-      default: () => [],
+      default: false,
     },
   },
   computed: {
@@ -123,18 +118,18 @@ export default {
        */
       if (typeof this.selected === 'boolean') {
         this.$emit('input', !this.selected);
-      } else {
-        // Store selected w/ spread operator on selected keys in array
-        let selected = [...this.selected];
-        if (selected.includes(this.value)) {
-          // uncheck
-          selected = selected.filter((value) => value !== this.value);
-        } else {
-          // check
-          selected.push(this.value);
-        }
-        this.$emit('input', selected);
+        return;
       }
+      // Store selected w/ spread operator on selected keys in array
+      let selected = [...this.selected];
+      if (selected.includes(this.value)) {
+        // uncheck
+        selected = selected.filter((value) => value !== this.value);
+      } else {
+        // check
+        selected.push(this.value);
+      }
+      this.$emit('input', selected);
     },
   },
 };
@@ -143,79 +138,75 @@ export default {
 <style lang="scss">
 
 .checkbox {
-    @apply relative cursor-pointer;
+  @apply cursor-pointer font-lato flex items-center text-sm text-gray-600 cursor-pointer;
 
-    &__container {
-       @apply flex items-center font-lato text-sm text-gray-600 cursor-pointer;
+  &__input {
+    @apply
+      flex
+      align-middle
+      m-0
+      relative
+      appearance-none
+      bg-white
+      rounded-sm
+      border border-gray-400
+      shadow-sm
+      h-4 w-4;
+
+    &:focus {
+      @apply outline-none;
     }
 
-    &__input {
-      @apply
-        flex
-        align-middle
-        m-0
-        relative
-        appearance-none
-        bg-white
-        rounded-sm
-        border border-gray-400
-        shadow-sm
-        h-4 w-4;
-
-      &:focus {
-        @apply outline-none;
-      }
-
-      &:hover{
-        @apply cursor-pointer bg-white border border-gray-500;
-      }
-
-      &:active{
-        @apply bg-gray-300 border border-gray-500;
-      }
-
-      &:disabled{
-        @apply bg-white border border-gray-300 cursor-not-allowed;
-      }
-
-      &:checked:after {
-        @apply text-white absolute;
-      }
-
-      &:checked{
-        @apply bg-gray-500 border-none;
-      }
-
-      &:indeterminate:after {
-        @apply text-white absolute;
-      }
-
-      &:indeterminate {
-        @apply bg-gray-500 border-none;
-      }
+    &:hover{
+      @apply cursor-pointer bg-white border border-gray-500;
     }
 
-    &__check {
-      @apply flex items-center justify-center absolute h-4 w-4;
-      svg {
-        @apply text-white h-3 w-3;
-      }
+    &:active{
+      @apply bg-gray-300 border border-gray-500;
     }
 
-    &__minus {
-      @apply flex items-center justify-center absolute h-4 w-4;
-      svg {
-        @apply text-white h-3 w-3;
-      }
+    &:disabled{
+      @apply bg-white border border-gray-300 cursor-not-allowed;
     }
 
-    &__label {
-      @apply ml-3;
+    &:checked:after {
+      @apply text-white absolute;
     }
 
-    &__label.disabled {
-      @apply text-gray-300 cursor-not-allowed;
+    &:checked{
+      @apply bg-gray-500 border-none;
     }
+
+    &:indeterminate:after {
+      @apply text-white absolute;
+    }
+
+    &:indeterminate {
+      @apply bg-gray-500 border-none;
+    }
+  }
+
+  &__check {
+    @apply flex items-center justify-center absolute h-4 w-4;
+    svg {
+      @apply text-white h-3 w-3;
+    }
+  }
+
+  &__minus {
+    @apply flex items-center justify-center absolute h-4 w-4;
+    svg {
+      @apply text-white h-3 w-3;
+    }
+  }
+
+  &__label {
+    @apply ml-3;
+  }
+
+  &__label.disabled {
+    @apply text-gray-300 cursor-not-allowed;
+  }
 }
 
 </style>
