2309041dffa551c3d73e958aee6bdd63bf2ce3e5
Robbie Averill <robbie@chec.io>
2020-03-13 15:34:37 -0700

    Card hologram background is now inlined as base64 encoded image data

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const assets = require('postcss-assets');
 
 module.exports = {
   plugins: [
     tailwindcss,
     autoprefixer,
+    assets,
   ],
 };
