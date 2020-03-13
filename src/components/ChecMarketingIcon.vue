6b7338b6c31286e048e0306ac9337308d6f86b0e
Jaeriah Tay <jaeriahpasseri@gmail.com>
2020-03-13 15:12:55 -0700

    recommitting changes to ChecMarketingIcon - changed all 'm' prefix to marketing

---
+++ src/components/ChecMarketingIcon.vue
@@ -0,0 +1,22 @@
+<script>
+import { mIcons } from '../lib/icons';
+
+export default {
+  name: 'ChecMarketingIcon',
+  props: {
+    /**
+     * Type of icon
+     */
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+  render(createElement) {
+    if (!mIcons[this.icon]) {
+      throw Error('Required param "icon" was not valid');
+    }
+    return createElement(mIcons[this.icon]);
+  },
+};
+</script>
