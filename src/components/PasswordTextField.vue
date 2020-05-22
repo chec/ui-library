5fb295a7ff8e3729f05afae42f9070002b896fc6
John Ray <john@trychec.com>
2020-05-22 17:44:09 -0400

    [CHEC-552] Create PasswordTextField, implementing show-password

---
+++ src/components/PasswordTextField.vue
@@ -0,0 +1,51 @@
+<template>
+  <TextField
+    v-bind="{...$attrs, ...$props}"
+    :type="passwordType"
+    :actionLabel="showHideText"
+    @onActionClick="toggleShowPassword"
+  />
+</template>
+<script>
+import TextField from './TextField.vue';
+
+export default {
+  name: 'PasswordTextField',
+  components: {
+    TextField,
+  },
+  data() {
+    return {
+      showPassword: this.initialShowPassword,
+    };
+  },
+  props: {
+    /**
+     * Default boolean value used for show/hide password state, false by default
+     */
+    initialShowPassword: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    isPasswordType() {
+      return this.$attrs.type === 'password';
+    },
+    showHideText() {
+      return `${this.showPassword ? 'hide' : 'show'} password`;
+    },
+    passwordType() {
+      return this.showPassword ? 'text' : 'password';
+    },
+  },
+  methods: {
+    /**
+     * Toggles password type with type text to show password
+     */
+    toggleShowPassword() {
+      this.showPassword = !this.showPassword;
+    },
+  },
+};
+</script>
--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -23,6 +23,13 @@
     <div class="text-field__right-content" ref="rightContentSlot">
       <slot></slot>
     </div>
+    <a
+      v-if="actionLabel"
+      class="text-field__action-button"
+      @click="onActionClick"
+    >
+      {{ actionLabel }}
+    </a>
   </div>
 </template>
 <script>
@@ -67,6 +74,13 @@ export default {
     multiline: {
       type: Boolean,
       default: false,
+    },
+    /**
+     * Text for action button beneath input
+     */
+    actionLabel: {
+      type: String,
+      default: '',
     },
     /**
      * Additional input attributes that should be applied to the native input
@@ -155,6 +169,14 @@ export default {
     },
   },
   methods: {
+    onActionClick(e) {
+      /**
+       * Emitted when action button is clicked
+       * @event click
+       * @type {e}
+       */
+      this.$emit('onActionClick', e);
+    },
     handleInput($event) {
       /**
        * Emitted when the `<input>`'s 'input' event bubbles up. The v-model directive uses this to function.
@@ -188,6 +210,10 @@ export default {
 <style lang="scss">
 .text-field {
   @apply relative;
+
+  &__action-button {
+    @apply title-xxs text-blue-500 float-right mt-1 cursor-pointer;
+  }
 
   &__label {
     @apply absolute top-0 left-0 cursor-text;
