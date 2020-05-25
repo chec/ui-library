c2080f9a161adb4b1acca942015e2a44781cdf08
Guy Marriott <guy@chec.io>
2020-05-25 16:54:59 -0700

    Add card header variant to existing header component

--- src/components/ChecHeader.vue
+++ src/components/ChecHeader.vue
@@ -1,8 +1,8 @@
 <template>
-  <header class="chec-header">
-    <h2 class="chec-header__title">
+  <header class="chec-header" :class="`chec-header--${this.variant}`">
+    <component :is="actualHeaderTag" class="chec-header__title">
       {{ title }}
-    </h2>
+    </component>
 
     <div v-if="label || $slots.default" class="chec-header-inner">
       <label class="chec-header-inner__label" v-if="label">
@@ -21,18 +21,35 @@ export default {
   name: 'ChecHeader',
   props: {
     /**
-     * heading text
+     * The text to be shown as the header
      */
     title: {
       type: String,
-      default: '',
+      required: true,
     },
     /**
-     * label text
+     * Optional text to be shown as a label, that appears before the optional slot
      */
     label: {
       type: String,
       default: '',
+    },
+    /**
+     * The type of header - either for a "section" or a "card"
+     */
+    variant: {
+      type: String,
+      validate: candidate => ['section', 'card'].includes(candidate),
+      default: 'section',
+    },
+    /**
+     * The HTML tag to use for the title of the section. Defaults to `h2` for a "section" and `h3` for a "card"
+     */
+    headerTag: String,
+  },
+  computed: {
+    actualHeaderTag() {
+      return this.headerTag || (this.variant === 'section' ? 'h2' : 'h3');
     },
   },
 };
@@ -40,9 +57,15 @@ export default {
 <style lang="scss">
 .chec-header {
   @apply flex justify-between items-center w-full;
-  &__title {
+
+  &--section &__title {
     @apply text-2xl;
   }
+
+  &--card &__title {
+    @apply rounded-full uppercase tracking-widest text-xxs bg-gray-200 px-2 py-1 text-gray-500;
+  }
+
   &-inner {
     @apply flex items-center;
     &__label {
