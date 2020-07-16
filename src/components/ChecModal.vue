0f110fbe77a5dcf9ba8feb319b2cdc7719478e97
kvisca <kyle@viscadesign.ca>
2020-07-16 15:27:37 -0400

    linting issue

--- src/components/ChecModal.vue
+++ src/components/ChecModal.vue
@@ -89,7 +89,8 @@ export default {
   &__overlay {
     @apply fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center overflow-scroll;
     background-color: rgba(255, 255, 255, 0.95);
-    &--dark{
+
+    &--dark {
       background-color: rgba(65, 85, 108, 0.9);
     }
   }
