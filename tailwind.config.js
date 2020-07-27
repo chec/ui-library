36b89a6e581d03e48f6c7ae31231ad8751646598
John Ray <john@trychec.com>
2020-07-27 17:31:13 -0400

    add shadow-holo

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,6 +52,9 @@ module.exports = {
       lg: '0px 4px 16px rgba(40, 51, 65, 0.1), 0px 0px 1px rgba(40, 51, 65, 0.2)',
       inner: 'inset 0px 1px 2px rgba(40, 51, 65, 0.1)',
       none: 'none',
+      holo: '4px -4px 16px rgba(156, 139, 218, 0.1), -4px -4px 16px rgba(115, 200, 242, 0.1),'
+      + '-4px 4px 16px rgba(133, 224, 206, 0.1), 4px 4px 16px rgba(231, 126, 144, 0.1),'
+      + '0px 4px 12px rgba(40, 51, 65, 0.1), 0px 0px 4px rgba(40, 51, 65, 0.1)',
     },
     colors: {
       transparent: 'transparent',
