75f38dbaed093c5a6cbdcfd5216266fabe417392
John Raymon <john@johnraymon.com>
2020-08-10 22:46:04 -0400

    create light, dark variants of chec-loading, and create prop to remove the background-overlay

--- src/components/ChecLoading.vue
+++ src/components/ChecLoading.vue
@@ -1,27 +1,21 @@
 <template>
-  <div class="loading">
+  <div class="loading" :class="classObject">
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      shape-rendering="geometricPrecision"
-      text-rendering="geometricPrecision"
       viewBox="0 0 32 32"
+      fill="none"
       class="loading__animation"
     >
-      <ellipse
-        rx="14"
-        ry="14"
-        fill="none"
-        stroke="#f1f6fc"
-        stroke-width="4"
-        stroke-dashoffset="400"
-        stroke-dasharray="100"
-        transform="translate(16,16) rotate(90)"
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28C22.6274 28 28 22.6274 28 16C28
+        9.37258 22.6274 4 16 4ZM0 16C0 7.16344 7.16344 0 16 0C24.8366 0 32 7.16344 32 16C32 24.8366
+        24.8366 32 16 32C7.16344 32 0 24.8366 0 16Z"
+        class="loading__spinner-background"
       />
       <ellipse
         rx="14"
         ry="14"
-        fill="none"
-        stroke="#34cbad"
         stroke-width="4"
         stroke-dashoffset="100"
         stroke-dasharray="100"
@@ -43,6 +37,24 @@ export default {
       type: String,
       default: '',
     },
+    variant: {
+      type: String,
+      default: 'default',
+      validator: function validator(value) {
+        return ['dark', 'light'].includes(value);
+      },
+    },
+    withoutBackground: {
+      type: Boolean,
+    },
+  },
+  computed: {
+    classObject() {
+      return [
+        this.withoutBackground ? 'loading--without-background' : 'loading--with-background',
+        this.variant !== 'default' ? `loading--${this.variant}` : '',
+      ];
+    },
   },
 };
 </script>
@@ -50,20 +62,56 @@ export default {
 <style scoped lang="scss">
 .loading {
   @apply absolute top-0 left-0 w-full h-full z-50 flex justify-center items-center flex-col;
-  // Custom: Overlay / Light
-  background: rgba(255, 255, 255, 0.9);
   border-radius: inherit;
 
   &__animation {
     @apply w-8;
   }
 
+  &__spinner-background {
+    @apply text-gray-200 fill-current;
+  }
+
   &__animation-spinner {
+    @apply text-gray-500 stroke-current;
     animation: 2.4s linear infinite both a0_t, 2.4s linear infinite both a0_do;
   }
 
   &__message {
     @apply caps-xs mt-2;
+  }
+
+  &--without-background {
+    @apply bg-transparent;
+  }
+
+  &--with-background {
+    background: rgba(255, 255, 255, 0.9);
+
+    &.loading--dark,
+    &.loading--light {
+      background: rgba(113, 135, 165, 0.9);
+    }
+  }
+
+  &--dark {
+    .loading__spinner-background {
+      @apply fill-current text-gray-600;
+    }
+
+    .loading__animation-spinner {
+      @apply stroke-current text-white;
+    }
+  }
+
+  &--light {
+    .loading__animation-spinner {
+      @apply stroke-current text-white;
+    }
+
+    .loading__spinner-background {
+      @apply fill-current text-gray-500;
+    }
   }
 }
 
