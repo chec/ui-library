4e0cb398b98472f3939414ef44a7a768af68f6f5
Jaeriah Tay <jaeriahpasseri@gmail.com>
2020-08-24 14:04:46 -0700

    add vertical nav component

---
+++ src/components/ChecVerticalNavItem.vue
@@ -0,0 +1,72 @@
+<template>
+  <li
+    class="vertical-navigation-item"
+    :class="{ 'vertical-navigation-item--active': active }"
+    @keyup.down="focusNextItem"
+    @keyup.up="focusPreviousItem"
+    @click="handleClick"
+  >
+    <a :href="link">
+      <!--
+        @slot Label to display as the navigation item label
+      -->
+      <slot v-bind="{ active, disabled }" />
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ChecVerticalNavItem',
+  props: {
+    /**
+     * Determines the link of the navigation item
+     */
+    link: {
+      type: String,
+      required: true,
+    },
+    /**
+     * If this navigation item should appear selected (active)
+     */
+    active: Boolean,
+    disabled: Boolean,
+  },
+  methods: {
+    /**
+     * Emitted the navigation item is clicked
+     * @event click
+     * @type {$event}
+     */
+    handleClick() {
+      this.$emit('click');
+    },
+    // focusNextItem() {
+
+    // },
+    // focusPreviousItem() {
+
+    // }
+  },
+};
+</script>
+
+<style lang="scss">
+.vertical-navigation-item {
+  @apply px-2 py-3 border-gray-200 border-b rounded cursor-pointer;
+
+  &:hover {
+    @apply bg-gray-100;
+  }
+
+  &:disabled {
+    @apply opacity-50 cursor-not-allowed;
+  }
+
+  &--active,
+  &:active,
+  &:focus {
+    @apply bg-gray-200 font-bold;
+  }
+}
+</style>
---
+++ src/components/ChecVerticalNavigation.vue
@@ -0,0 +1,74 @@
+<template>
+  <nav class="vertical-navigation">
+    <ul
+      class="vertical-navigation__list"
+    >
+      <ChecVerticalNavItem
+        v-for="{ label, link, active } in options"
+        :key="label"
+        :disabled="disabled"
+        :active="active"
+        :link="link"
+        @keyup.down="focusNextItem"
+        @keyup.up="focusPreviousItem"
+        @click="handleClick"
+      >
+        {{ label }}
+      </ChecVerticalNavItem>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import ChecVerticalNavItem from './ChecVerticalNavItem';
+
+export default {
+  name: 'ChecVerticalNavigation',
+  components: {
+    ChecVerticalNavItem,
+  },
+  props: {
+    /**
+     * The options for the navigation items
+     */
+    options: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+    /**
+     * Disables the navigation item (but not the click handler)
+     */
+    disabled: Boolean,
+  },
+  methods: {
+    /**
+     * Emitted the navigation item is clicked
+     * @event click
+     * @type {$event}
+     */
+    handleClick() {
+      return this.active;
+    },
+    isActive(current) {
+      return this.active === current;
+    },
+    // focusNextItem() {
+
+    // },
+    // focusPreviousItem() {
+
+    // }
+  },
+};
+</script>
+
+<style lang="scss">
+.vertical-navigation {
+  @apply rounded shadow-sm w-56 bg-white px-3 py-5;
+
+  &__list {
+    @apply text-gray-500;
+  }
+}
+</style>
