ad06f5777bf8ed2e64685148a26dc28234dc18d3
kvisca <kyle@viscadesign.ca>
2020-09-11 13:21:23 -0400

    File setup for image uploader

---
+++ src/components/ChecImageUploader.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="chec-file-uploader">
+    <div class="chec-file-uploader__inner" data-dropzone-clickable>
+      <div v-if="files.length" class="file-rows-container space-y-2" @click.stop>
+        <ImageBlock
+          v-for="file in files"
+          :key="file.upload.uuid"
+          :error="file.status === 'error'"
+          :loading="file.upload.progress !== null && file.upload.progress < 100"
+          :file-name="file.name"
+          :file-size="file.size"
+          :mime-type="file.type"
+          :progress="file.upload.progress"
+          @remove-file="() => handleRemovingFile(file)"
+        />
+      </div>
+      <ChecButton data-dropzone-clickable @click.stop>
+        Choose file(s)
+      </ChecButton>
+      <input class="chec-file-uploader__input" type="file" name="file">
+    </div>
+  </div>
+</template>
+
+<script>
+import Dropzone from 'dropzone';
+import ChecButton from './ChecButton';
+import ImageBlock from './ChecImageUploader/ImageBlock.vue';
+
+Dropzone.autoDiscover = false;
+
+export default {
+  name: 'ChecImageUploader',
+  components: {
+    ChecButton,
+    ImageBlock,
+  },
+  props: {
+    /**
+     * The files that will be rendered as <file-row> components
+     */
+    files: {
+      type: Array,
+      default: () => [],
+    },
+    /**
+     * The endpoint to upload files to
+     */
+    endpoint: {
+      type: String,
+      default: '/',
+    },
+  },
+  data() {
+    return {
+      inProgressFiles: [],
+    };
+  },
+  mounted() {
+    this.dropzone = new Dropzone(this.$el, {
+      url: this.endpoint,
+      createImageThumbnails: false,
+      previewsContainer: false,
+      clickable: '[data-dropzone-clickable]',
+      hiddenInputContainer: this.$el,
+    });
+    const vm = this;
+    this.dropzone.on('complete', (file) => {
+      vm.inProgressFiles = [
+        ...vm.inProgressFiles,
+        file,
+      ];
+      this.emitFileUploadingComplete(file);
+    });
+    this.dropzone.on('addedfile', (file) => {
+      vm.inProgressFiles = [
+        ...vm.inProgressFiles,
+        file,
+      ];
+      this.emitFileAdded(file);
+    });
+    this.dropzone.on('uploadprogress', (file) => {
+      vm.inProgressFiles = [
+        ...vm.inProgressFiles,
+        file,
+      ];
+      this.emitFileUploading(file);
+    });
+  },
+  methods: {
+    handleRemovingFile(file) {
+      /**
+       * Emitted when the file has been removed
+       *
+       * @event remove-file
+       * @type {Object}
+       */
+      this.$emit('remove-file', this.getNonReactiveFileObject(file));
+    },
+    getNonReactiveFileObject(file) {
+      return {
+        upload: { ...file.upload }, name: file.name, size: file.size, type: file.type, status: file.status,
+      };
+    },
+    emitFileUploading(file) {
+      /**
+       * Emitted when the file has begun uploading, derived from Dropzone.js' uploadprogress event
+       *
+       * @event file-uploading
+       * @type {Object}
+       */
+      this.$emit('file-uploading', this.getNonReactiveFileObject(file));
+    },
+    emitFileAdded(file) {
+      /**
+       * Emitted when the file has been initially added, derived from Dropzone.js' addedfile event
+       * @event file-added
+       * @type {$event}
+       */
+      this.$emit('file-added', this.getNonReactiveFileObject(file));
+    },
+    emitFileUploadingComplete(file) {
+      /**
+       * Emitted when the file has completed uploading, derived from Dropzone.js' file-uploading-complete event
+       * @event file-uploading-complete
+       * @type {$event}
+       */
+      this.$emit('file-uploading-complete', this.getNonReactiveFileObject(file));
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.chec-file-uploader {
+  @apply relative flex flex-col w-full h-auto flex rounded shadow-inner bg-gray-gradient flex justify-center;
+
+  &__inner {
+    @apply relative border border-dashed border-gray-400 rounded m-2 p-8;
+  }
+
+  .button {
+    @apply mx-auto;
+  }
+
+  &__input {
+    display: none;
+  }
+}
+
+.file-rows-container {
+  @apply w-full flex flex-col mb-2;
+}
+</style>
---
+++ src/components/ChecImageUploader/ImageBlock.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="chec-file-row" :class="{ 'chec-file-row--error': error }">
+    <div class="chec-file-row__left">
+      <div v-if="loading && !error" class="chec-file-row__loading-icon">
+        <ChecLoading without-background variant="dark" />
+      </div>
+      <div v-else class="chec-file-row__icon">
+        <ChecIcon
+          v-if="error"
+          icon="close"
+        />
+        <ChecIcon
+          v-else
+          icon="check"
+        />
+      </div>
+      <p v-if="!error" class="chec-file-row__label">
+        {{ fileName || '--' }}
+      </p>
+      <p v-else class="chec-file-row__label">
+        {{ `${fileName || '' } - ${errorMessage || 'Failed' }` }}
+      </p>
+    </div>
+    <div class="chec-file-row__right">
+      <p v-if="loading" class="chec-file-row__progress">
+        {{ progressPercent }}
+      </p>
+      <template v-else>
+        <p v-if="!error" class="chec-file-row__type">
+          {{ mimeType }}
+        </p>
+        <p v-if="!error" class="chec-file-row__size">
+          {{ size }}
+        </p>
+      </template>
+      <button class="chec-file-row__remove-button" @click="() => $emit('remove-file')">
+        <ChecIcon
+          icon="close"
+        />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import filesize from 'filesize';
+import ChecIcon from '../ChecIcon';
+import ChecLoading from '../ChecLoading';
+
+export default {
+  name: 'ImageItem',
+  components: {
+    ChecIcon,
+    ChecLoading,
+  },
+  props: {
+    /**
+     * The uploading progress percentage
+     */
+    progress: {
+      type: Number,
+      default: 0,
+    },
+    /**
+     * Boolean to indicate a loading state
+     */
+    loading: Boolean,
+    /**
+     * Boolean to render the error state
+     */
+    error: Boolean,
+    /**
+     * Optional error message
+     */
+    errorMessage: String,
+    /**
+     * The name of the file
+     */
+    fileName: {
+      type: String,
+      default: '',
+    },
+    /**
+     * The file's media type
+     */
+    mimeType: {
+      type: String,
+      default: '',
+    },
+    /**
+     * The file's size, rendered as human readable file size string using the npm lib. filesize
+     */
+    fileSize: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    size() {
+      return filesize(this.fileSize);
+    },
+    progressPercent() {
+      return `${(this.progress).toFixed()}%`;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.chec-file-row {
+  @apply flex justify-between w-full p-4 rounded bg-gray-500;
+
+  &__loading-icon {
+    @apply relative mr-1 h-5 w-5;
+  }
+
+  &__left {
+    @apply flex items-center;
+  }
+
+  &__right {
+    @apply flex items-center justify-end;
+  }
+
+  &__icon {
+    @apply w-5 h-5 p-1 mr-1 text-white;
+  }
+
+  &__remove-button {
+    @apply w-4 h-4 mr-1 bg-gray-100 text-gray-500 rounded-full;
+
+    &:focus,
+    :hover {
+      @apply opacity-75 outline-none;
+    }
+  }
+
+  &__label {
+    @apply text-white text-xs;
+  }
+
+  &__type {
+    @apply text-white caps-xxs mr-4;
+    letter-spacing: 1px;
+  }
+
+  &__size,
+  &__progress {
+    @apply text-white caps-xxs mr-4;
+    letter-spacing: 1px;
+  }
+
+  &--error {
+    @apply bg-gray-100 border border-gray-300 text-red-600;
+
+    .chec-file-row {
+      &__icon {
+        @apply text-red-600;
+      }
+
+      &__label {
+        @apply text-red-600;
+      }
+
+      &__remove-button {
+        @apply bg-red-500 text-white;
+      }
+    }
+  }
+}
+</style>
