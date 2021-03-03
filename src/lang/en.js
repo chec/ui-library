05d37135afe090868c7ebef9a0eac01e3c0c04ea
kvisca <kyle@viscadesign.ca>
2021-03-03 17:01:29 -0500

    Integrating localazy with UI LIibrary

--- src/lang/en.js
+++ src/lang/en.js
@@ -1,60 +1,58 @@
 export default {
-  en: {
-    accordion: {
-      switchLabel: {
-        open: 'Enabled',
-        closed: 'Disabled',
-      },
-      toggleLabel: {
-        open: 'Hide this section',
-        closed: 'Show this section',
-      },
+  accordion: {
+    switchLabel: {
+      open: 'Enabled',
+      closed: 'Disabled',
     },
-    general: {
-      search: 'Search',
-      // Appended to required form field labels
-      requiredInline: '(required)',
-      // Used after other statements, like in the case of filters: "Refunded: yes"
-      yes: 'yes',
+    toggleLabel: {
+      open: 'Hide this section',
+      closed: 'Show this section',
     },
-    fileManager: {
-      chooseFiles: 'Choose file(s)',
-      deleteFile: 'Delete file',
-    },
-    filters: {
-      apply: 'Apply filters',
-      clear: 'Clear',
-      filters: 'Filters',
-      textSearch: 'Text search',
-    },
-    imageManager: {
-      chooseImages: 'Choose image(s)',
-      deleteImage: 'Delete image',
-    },
-    navigation: {
-      returnToHome: 'Return to the home page',
-      skip: 'Skip navigation',
-    },
-    paginate: {
-      goToFirst: 'Go to the first page',
-      goToNext: 'Go to the next page',
-      goToPrevious: 'Go to the previous page',
-      goToLast: 'Go to the last page',
-      showing: 'Showing',
-      of: '{x} of {y}',
-    },
-    tag: {
-      dismiss: 'Dismiss',
-      reset: 'Reset',
-    },
-    wysiwyg: {
-      bold: 'Bold',
-      italic: 'Italic',
-      h1: 'Header 1',
-      h2: 'Header 2',
-      h3: 'Header 3',
-      blockquote: 'Blockquote',
-      bulletlist: 'Bullet list',
-    },
+  },
+  general: {
+    search: 'Search',
+    // Appended to required form field labels
+    requiredInline: '(required)',
+    // Used after other statements, like in the case of filters: "Refunded: yes"
+    yes: 'yes',
+  },
+  fileManager: {
+    chooseFiles: 'Choose file(s)',
+    deleteFile: 'Delete file',
+  },
+  filters: {
+    apply: 'Apply filters',
+    clear: 'Clear',
+    filters: 'Filters',
+    textSearch: 'Text search',
+  },
+  imageManager: {
+    chooseImages: 'Choose image(s)',
+    deleteImage: 'Delete image',
+  },
+  navigation: {
+    returnToHome: 'Return to the home page',
+    skip: 'Skip navigation',
+  },
+  paginate: {
+    goToFirst: 'Go to the first page',
+    goToNext: 'Go to the next page',
+    goToPrevious: 'Go to the previous page',
+    goToLast: 'Go to the last page',
+    showing: 'Showing',
+    of: '{x} of {y}',
+  },
+  tag: {
+    dismiss: 'Dismiss',
+    reset: 'Reset',
+  },
+  wysiwyg: {
+    bold: 'Bold',
+    italic: 'Italic',
+    h1: 'Header 1',
+    h2: 'Header 2',
+    h3: 'Header 3',
+    blockquote: 'Blockquote',
+    bulletlist: 'Bullet list',
   },
 };
