2996b2e8d9ae7dfc1ee0fef891770acba1d039d8
Guy Marriott <guy@chec.io>
2021-03-26 11:12:55 -0700

    Remove unused theme classes from tooltips

--- src/directives/ChecTooltip.js
+++ src/directives/ChecTooltip.js
@@ -1,6 +1,10 @@
 import '../assets/tooltips.scss';
 import { VPopover, VClosePopover, VTooltip } from 'v-tooltip';
 
+// Remove unused "theme" classes from the tooltips
+VTooltip.options.defaultClass = '';
+VTooltip.options.popover.defaultClass = '';
+
 export default {
   VPopover,
   VClosePopover,
