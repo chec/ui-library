6f741f263bd83a382a84e257ef3ee74dcfdd76e9
kvisca <kyle@viscadesign.ca>
2021-08-24 11:04:57 -0400

    Adding inline variant of vertical navigation

--- src/components/ChecVerticalNavigation.vue
+++ src/components/ChecVerticalNavigation.vue
@@ -1,5 +1,8 @@
 <template>
-  <nav class="vertical-navigation">
+  <nav
+    class="vertical-navigation"
+    :class="{ 'vertical-navigation--inline': inline}"
+  >
     <ul
       class="vertical-navigation__list"
     >
@@ -15,12 +18,22 @@
 
 export default {
   name: 'ChecVerticalNavigation',
+  props: {
+    /**
+     * If this navigation list should appear inline
+     */
+    inline: Boolean,
+  },
 };
 </script>
 
 <style lang="scss">
 .vertical-navigation {
   @apply rounded-md shadow-sm bg-white px-3 py-3;
+
+  &--inline {
+    @apply py-0 px-0 shadow-none bg-transparent rounded-none;
+  }
 
   &__list {
     @apply text-gray-500;
